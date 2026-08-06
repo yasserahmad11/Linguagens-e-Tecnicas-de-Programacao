101dc6b757e4a4c7d173ee9a083a3f85d5a7b538
Yasser Ahmad <yasserahmad2404@gmail.com>
2026-08-06 11:38:11 -0300

    Add files via upload

---
+++ Aula2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]){
+
+    int a,b,c,d,e,f;
+    a = 8;
+    b = 19;
+    c = a+b;
+    d = a-b;
+    e = a/b;
+    f = a*b;
+
+    printf("As operações de %d %d são: %d %d %d %d", a,b,c,d,e,f);
+
+    return 0;
+
+
+}
