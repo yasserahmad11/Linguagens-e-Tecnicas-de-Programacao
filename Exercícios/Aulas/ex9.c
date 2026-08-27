fd61f2cdeb5b9875e06b53a51492b14316c8197d
Yasser Ahmad <yasserahmad2404@gmail.com>
2026-08-27 11:20:07 -0300

    Implement prime number check and area calculation
    Added conditions to check if numbers are prime and within range.

--- Exercícios/Aulas/ex9.c
+++ Exercícios/Aulas/ex9.c
@@ -11,6 +11,7 @@ int main(){
     printf("Digite outro número: ");
     scanf("%d", &num2);
 
+    //Condições
     if(num1 >= 0 && num2 >= 0){
         if(num1 && num2 < 10){
             if ((num1 == 2 || num1 == 3 || num1 == 5 || num1 == 7) && (num2 == 2 || num2 == 3 || num2 == 5 || num2 == 7)){
